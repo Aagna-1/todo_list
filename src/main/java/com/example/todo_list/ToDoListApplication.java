ffcaf8dca9ba4377aa827ee5765688c0ed478212
Kaushik Modi <kaushikmodi@Kaushiks-Air.attlocal.net>
2025-05-13 18:32:10 -0700

    Renamed the fxml/view file

--- src/main/java/com/example/todo_list/ToDoListApplication.java
+++ src/main/java/com/example/todo_list/ToDoListApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class ToDoListApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ToDoListApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(ToDoListApplication.class.getResource("todo-list-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Aagna's To-Do List");
         stage.setScene(scene);
